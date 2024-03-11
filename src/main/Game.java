0b7fba46a1e26d035ce3dffdbfab68e5bde7d762
Alinur <125266339+aibraikhan@users.noreply.github.com>
2024-03-11 18:53:57 +0600

    Animation: attacks, improved - moving

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,9 @@
 package src.main;
 
+import java.awt.Graphics;
+
+import entities.Player;
+
 public class Game implements Runnable {
 
     private GameWindow gameWindow;
@@ -8,12 +12,21 @@ public class Game implements Runnable {
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
 
+    private Player player;
+
     public Game() {
-        gamePanel = new GamePanel();
+        initClasses();
+
+        gamePanel = new GamePanel(this);
         gameWindow = new GameWindow(gamePanel);
         gamePanel.setFocusable(true);
         gamePanel.requestFocusInWindow();
+
         startGameLoop();
+    }
+
+    private void initClasses() {
+        player = new Player(200, 200);
     }
 
     private void startGameLoop() {
@@ -22,7 +35,11 @@ public class Game implements Runnable {
     }
 
     public void update() {
-        gamePanel.updateGame();
+        player.update();
+    }
+
+    public void render(Graphics g) {
+        player.render(g);
     }
 
     @Override
@@ -69,5 +86,12 @@ public class Game implements Runnable {
 
         }
     }
+    
+    public void windowFocusLost() {
+        player.resetDirBooleans();
+    }
 
+    public Player getPlayer() {
+        return player;
+    }
 }
