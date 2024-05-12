107e3808bcd702e18ab1c6291734fdbe40febdfd
Alinur <125266339+aibraikhan@users.noreply.github.com>
2024-05-12 15:43:45 +0600

    I've added a lot of things

---
+++ src/entities/Crabby.java
@@ -0,0 +1,9 @@
+package src.entities;
+
+import static src.utilz.Constants.EnemyConstants.*;
+
+public class Crabby extends Enemy {
+    public Crabby(float x, float y) {
+        super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+    }
+}
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -4,22 +4,31 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import src.entities.Crabby;
 import src.main.Game;
+
+import static src.utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
     public static final String PLAYER_ATLAS = "res/player_sprites.png";
     public static final String LEVEL_ATLAS = "res/outside_sprites.png";
-    public static final String LEVEL_ONE_DATA = "res/level_one_data.png";
+    public static final String LEVEL_ONE_DATA = "res/level_one_data_long.png";
     public static final String MENU_BUTTONS = "res/button_atlas.png";
     public static final String MENU_BACKGROUND = "res/menu_background.png";
     public static final String PAUSE_BACKGROUND = "res/pause_menu.png";
     public static final String SOUND_BUTTONS = "res/sound_button.png";
     public static final String URM_BUTTONS = "res/urm_buttons.png";
     public static final String VOLUME_BUTTONS = "res/volume_buttons.png";
+    public static final String MENU_BACKGROUND_IMG = "res/ostrov_korabl.jpg";
+    public static final String PLAYING_BG_ING = "res/playing_bg_img.png";
+    public static final String BIG_CLOUDS = "res/big_clouds.png";
+    public static final String SMALL_CLOUDS = "res/small_clouds.png";
+    public static final String CRABBY_SPRITE = "res/crabby_sprite.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
@@ -38,9 +47,23 @@ public class LoadSave {
         return img;
     }
 
-    public static int[][] GetLevelData() {
-        int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+    public static ArrayList<Crabby> GetCrabs() {
         BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+        ArrayList<Crabby> list = new ArrayList<>();
+
+        for (int j = 0; j < img.getHeight(); j++)
+            for (int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getGreen();
+                if (value == CRABBY)
+                    list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE));
+            }
+        return list;
+    }
+
+    public static int[][] GetLevelData() {
+        BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+        int[][] lvlData = new int[img.getHeight()][img.getWidth()];
 
         for (int j = 0; j < img.getHeight(); j++) {
             for (int i = 0; i < img.getWidth(); i++) {
